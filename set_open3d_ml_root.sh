4a3459bc7e27c6994ba76e840d4278c75179ba73
m-klasen <morris.klasen@gmail.com>
2021-06-25 10:05:27 +0200

    Update

--- set_open3d_ml_root.sh
+++ set_open3d_ml_root.sh
@@ -1,6 +1,6 @@
 # Sets the env var OPEN3D_ML_ROOT to the directory of this file.
 # The open3d package will use this var to integrate ml3d into a common namespace.
-export OPEN3D_ML_ROOT=/home/mlk/AMMOD/code/learning_3dfold/Open3D-ML
+export OPEN3D_ML_ROOT=/home/mlk/AMMOD/code/3d_tracking/Open3D-ML
 # if [[ $0 == $BASH_SOURCE ]]; then 
 #         echo "source this script to set the OPEN3D_ML_ROOT env var."     
 # else
